24ceb4dba389d85a4c6c1a4aafbe143cc1b414d4
Gregory Nickonov <gregoryn@actis.ru>
2013-02-07 21:52:19 +0400

    NavigationCommand is now available for all target projects.

---
+++ src/Digillect.MVVM/NavigationCommand`1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Windows.Input;
+
+using Digillect.Mvvm.Services;
+
+namespace Digillect.Mvvm
+{
+	/// <summary>
+	/// Command that performs navigation to the specified view.
+	/// </summary>
+	public class NavigationCommand<T> : ICommand
+	{
+		private readonly INavigationService _navigationService;
+		private readonly string _view;
+		private readonly Func<T, bool> _canNavigate;
+		private readonly Func<T, Parameters> _parametersProvider;
+
+		#region Constructors/Disposer
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NavigationCommand" /> class.
+		/// </summary>
+		/// <param name="navigationService">Instance of navigation service to be used to perform navigation.</param>
+		/// <param name="view">Target view.</param>
+		public NavigationCommand( INavigationService navigationService, string view )
+			: this( navigationService, view, null, (Func<T, Parameters>) null )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NavigationCommand" /> class.
+		/// </summary>
+		/// <param name="navigationService">Instance of navigation service to be used to perform navigation.</param>
+		/// <param name="view">Target view.</param>
+		/// <param name="canNavigate">Function that determines whether it is possible to navigate to the specified view.</param>
+		public NavigationCommand( INavigationService navigationService, string view, Func<T, bool> canNavigate )
+			: this( navigationService, view, canNavigate, (Func<T, Parameters>) null )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NavigationCommand" /> class.
+		/// </summary>
+		/// <param name="navigationService">Instance of navigation service to be used to perform navigation.</param>
+		/// <param name="view">Target view.</param>
+		/// <param name="parameters">Parameters to pass to the target view.</param>
+		public NavigationCommand( INavigationService navigationService, string view, Parameters parameters )
+			: this( navigationService, view, null, item => parameters )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NavigationCommand" /> class.
+		/// </summary>
+		/// <param name="navigationService">Instance of navigation service to be used to perform navigation.</param>
+		/// <param name="view">Target view.</param>
+		/// <param name="canNavigate">Function that determines whether it is possible to navigate to the specified view.</param>
+		/// <param name="parameters">Parameters to pass to the target view.</param>
+		public NavigationCommand( INavigationService navigationService, string view, Func<T, bool> canNavigate, Parameters parameters )
+			: this( navigationService, view, canNavigate, item => parameters )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NavigationCommand" /> class.
+		/// </summary>
+		/// <param name="navigationService">Instance of navigation service to be used to perform navigation.</param>
+		/// <param name="view">Target view.</param>
+		/// <param name="parametersProvider">Function that returns parameters to pass to the target view.</param>
+		public NavigationCommand( INavigationService navigationService, string view, Func<T, Parameters> parametersProvider )
+			: this( navigationService, view, null, parametersProvider )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NavigationCommand" /> class.
+		/// </summary>
+		/// <param name="navigationService">Instance of navigation service to be used to perform navigation.</param>
+		/// <param name="view">Target view.</param>
+		/// <param name="canNavigate">Function that determines whether it is possible to navigate to the specified view.</param>
+		/// <param name="parametersProvider">Function that returns parameers to pass to the target view.</param>
+		public NavigationCommand( INavigationService navigationService, string view, Func<T, bool> canNavigate, Func<T, Parameters> parametersProvider )
+		{
+			if( navigationService == null )
+			{
+				throw new ArgumentNullException( "navigationService" );
+			}
+
+			if( view == null )
+			{
+				throw new ArgumentNullException( "view" );
+			}
+
+			Contract.EndContractBlock();
+
+			_navigationService = navigationService;
+			_view = view;
+			_canNavigate = canNavigate;
+			_parametersProvider = parametersProvider;
+		}
+		#endregion
+
+		/// <summary>
+		/// Occurs when changes occur that affect whether or not the command should execute.
+		/// </summary>
+		public event EventHandler CanExecuteChanged;
+
+		/// <summary>
+		/// Raises event that indicates that <see cref="CanExecute"/> return value has been changed.
+		/// </summary>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1030:UseEventsWhereAppropriate" )]
+		public void RaiseCanExecuteChanged()
+		{
+			var handler = CanExecuteChanged;
+
+			if( handler != null )
+			{
+				handler( this, EventArgs.Empty );
+			}
+		}
+
+		/// <summary>
+		/// Defines the method that determines whether the command can execute in its current state.
+		/// </summary>
+		/// <param name="parameter">Data used by the command. Always ignored in this implementation.</param>
+		/// <returns>
+		/// true if this command can be executed; otherwise, false.
+		/// </returns>
+		public bool CanExecute( object parameter )
+		{
+			return _canNavigate == null || _canNavigate( (T) parameter );
+		}
+
+		/// <summary>
+		/// Defines the method to be called when the command is invoked.
+		/// </summary>
+		/// <param name="parameter">Data used by the command. Always ignored in this implementation.</param>
+		public void Execute( object parameter )
+		{
+			if( CanExecute( parameter ) )
+			{
+				Parameters parameters = _parametersProvider == null ? null : _parametersProvider( (T) parameter );
+
+				_navigationService.Navigate( _view, parameters );
+			}
+		}
+	}
+}
