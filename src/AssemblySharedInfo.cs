fc2f919638bf882ace8a6dbc13ecc673ea9dcd1e
Alexander Stavnichiy <alexander.stavnichiy@actis.ru>
2014-07-01 12:56:52 +0400

    Update copyright.

--- src/AssemblySharedInfo.cs
+++ src/AssemblySharedInfo.cs
@@ -1,5 +1,5 @@
-﻿#region Copyright (c) 2011-2013 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman)
-// Copyright (c) 2011-2013 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman).
+﻿#region Copyright (c) 2011-2014 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman)
+// Copyright (c) 2011-2014 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman).
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany( "Actis Wunderman" )]
 [assembly: AssemblyProduct( "Digillect® Model-View-ViewModel Framework." )]
-[assembly: AssemblyCopyright( "© 2011-2013 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman). All rights reserved." )]
+[assembly: AssemblyCopyright( "© 2011-2014 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman). All rights reserved." )]
 [assembly: AssemblyTrademark( "Digillect is a registered trademark of Actis Wunderman." )]
 [assembly: AssemblyVersion( AssemblyInfo.Version )]
 [assembly: AssemblyFileVersion( AssemblyInfo.FileVersion )]
