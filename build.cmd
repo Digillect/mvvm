bcec7bd59c9c266cd959a3a75c0c07db11aff234
Andrew Nefedkin <andrew.nefedkin@actis.ru>
2015-11-05 12:01:01 +0300

    Updated build script.

--- build.cmd
+++ build.cmd
@@ -5,29 +5,24 @@ set EnableNuGetPackageRestore=true
 
 pushd %~dp0
 
-call :ResolveNuGet nuget.exe
-
-if errorlevel 1 (
-	echo NuGet.Exe was not found either in .nuget subfolder or PATH
-	exit /b %ERRORLEVEL%
-)
+call :ResolveNuGet nuget.exe || exit /b %ERRORLEVEL%
 
 if exist .nuget\packages.config (
-	"%NuGetExe%" restore .nuget\packages.config -PackagesDirectory packages -Verbosity quiet -NonInteractive
+	echo Restoring packages from %~dp0.nuget\packages.config
+	"%NuGetExe%" restore .nuget\packages.config -PackagesDirectory packages -Verbosity quiet -NonInteractive || exit /b %ERRORLEVEL%
 )
 
-if not errorlevel 1 (
-	%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /nologo /v:m %*
-)
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /nologo /v:m /p:NuGetExecutable="%NuGetExe%" %*
 
 popd
 goto :EOF
 
 :ResolveNuGet
-set NuGetExe=%~$PATH:1
-if not "%NuGetExe%"=="" goto :EOF
 if exist %CD%\.nuget\%1 (
 	set NuGetExe=%CD%\.nuget\%1
 	goto :EOF
 )
+set NuGetExe=%~$PATH:1
+if not "%NuGetExe%"=="" goto :EOF
+echo NuGet.Exe was not found either in .nuget subfolder or PATH
 exit /b 1
