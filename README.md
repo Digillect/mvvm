7c84f1e12216399853ed0ee9321539fe35a6aa15
Andrew Nefedkin <andrew.nefedkin@actis.ru>
2016-12-01 20:09:21 +0300

    PCL -> .NET Standard.

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿Digillect MVVM (Model-View-ViewModel)
+=====================================
+
+[![Build status](https://ci.appveyor.com/api/projects/status/bfrdok8578my2t94?svg=true)](https://ci.appveyor.com/project/Digillect/mvvm)
+[![NuGet](https://img.shields.io/nuget/v/Digillect.Mvvm.svg)](https://www.nuget.org/packages/Digillect.Mvvm)
--- build.cmd
+++ build.cmd
@@ -1,28 +1,42 @@
 @echo off
 
-setlocal enableextensions
-set EnableNuGetPackageRestore=true
+setlocal enableextensions enabledelayedexpansion
 
 pushd %~dp0
 
 call :ResolveNuGet nuget.exe || exit /b %ERRORLEVEL%
 
-if exist .nuget\packages.config (
-	echo Restoring packages from %~dp0.nuget\packages.config
-	"%NuGetExe%" restore .nuget\packages.config -Verbosity quiet -NonInteractive || exit /b %ERRORLEVEL%
+call :FindMSBuild 14.0
+
+if errorlevel 1 (
+	echo Could not find any applicable MSBuild version
+	exit /b %ERRORLEVEL%
 )
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /nologo /v:m /p:NuGetExecutable="%NuGetExe%" %*
+if exist .nuget\packages.config (
+	echo Restoring packages from !CD!\.nuget\packages.config
+	"%NuGetExe%" restore .nuget\packages.config -PackagesDirectory packages -NonInteractive -Verbosity quiet || exit /b !ERRORLEVEL!
+)
+
+"%MSBUILDDIR%MSBuild.exe" build.proj /nologo /v:m /p:SolutionDir="%~dp0\" /p:NuGetExecutable="%NuGetExe%" /p:NuGetVerbosity=quiet %*
 
 popd
 goto :EOF
 
 :ResolveNuGet
 if exist %CD%\.nuget\%1 (
-	set NuGetExe=%CD%\.nuget\%1
+	set NuGetExe=!CD!\.nuget\%1
 	goto :EOF
 )
 set NuGetExe=%~$PATH:1
 if not "%NuGetExe%"=="" goto :EOF
 echo NuGet.Exe was not found either in .nuget subfolder or PATH
+exit /b 1
+
+:FindMSBuild
+rem http://stackoverflow.com/a/20431996/393672
+reg.exe query "HKLM\SOFTWARE\WOW6432Node\Microsoft\MSBuild\ToolsVersions\%1" /v MSBuildToolsPath > nul 2>&1
+if errorlevel 1 exit /b %ERRORLEVEL%
+for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\WOW6432Node\Microsoft\MSBuild\ToolsVersions\%1" /v MSBuildToolsPath') do SET MSBUILDDIR=%%B
+if exist "%MSBUILDDIR%MSBuild.exe" goto :EOF
 exit /b 1
